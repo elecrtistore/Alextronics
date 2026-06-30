b0a3892f0748fbed64e6c8250c8462fdbd5982ab
elecrtistore <elecrtistore@users.noreply.github.com>
2026-06-30 12:15:27 +0300

    Fix GH Pages routing: add BrowserRouter basename, SPA 404 fallback

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
       <InquiryProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/shop">
           <App />
         </BrowserRouter>
       </InquiryProvider>
